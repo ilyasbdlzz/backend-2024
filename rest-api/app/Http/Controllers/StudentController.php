27a75eb62a353f949f620821ee48436aeaf939a9
ilyasbdlzz <ilyaskenway@gmail.com>
2024-10-26 21:29:50 +0700

    Controller & Routes Updated - Update and Delete

--- rest-api/app/Http/Controllers/StudentController.php
+++ rest-api/app/Http/Controllers/StudentController.php
@@ -49,18 +49,7 @@ class StudentController extends Controller
      */
     public function show(string $id)
     {
-        $student = Student::find($id);
-
-        if (!$student) {
-            return response()->json(['message' => 'Student not found'], 404);
-        }
-
-        $response = [
-            'message' => 'Student data retrieved successfully',
-            'data' => $student
-        ];
-
-        return response()->json($response, 200);
+        //
     }
 
     /**
