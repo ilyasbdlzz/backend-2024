9754a14221f76dd7c9827c7ce75d2cacb1af3017
ilyasbdlzz <ilyaskenway@gmail.com>
2024-10-26 20:56:34 +0700

    Migrate database, Routes & Controller (index & Store)

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,3 +20,6 @@ Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 # method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+Route::get('students/', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
