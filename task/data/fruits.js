414a70cada0359b64695ae0705d3e62eba66ee9b
ilyasbdlzz <ilyaskenway@gmail.com>
2024-12-06 08:31:48 +0700

    Push praktikum 9

---
+++ task/data/fruits.js
@@ -0,0 +1,10 @@
+/**
+ * TODO 1:
+ * - Buat array yang berisi data buah.
+ * - Refactor variable ke ES6 Variable.
+ */
+const fruits = ['Semangka','Durian','Apel'];
+
+// TODO 2: export variable fruits
+module.exports = fruits;
+
